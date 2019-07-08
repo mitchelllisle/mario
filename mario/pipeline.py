a5fbb4f79e4afd962633c0d134e270bdbe75675f
Mitchell Lisle <lislemitchell@gmail.com>
2019-07-08 22:26:54 +0100

    added more data to pipeline

--- mario/pipeline.py
+++ mario/pipeline.py
@@ -1,3 +1,7 @@
+from uuid import uuid4
+import datetime as dt
+
+
 class Pipeline:
     def __init__(self, description=None):
         """
@@ -26,10 +30,17 @@ class Pipeline:
         if depends_on:
             idx = self.tasks.index(depends_on) + 1
 
-        def inner(f):
-            self.tasks.insert(idx, f)
-            return f
-        return inner
+        def build_function(instance):
+            func_meta = {
+                "task_id": str(uuid4()),
+                "function_name": instance.__name__,
+                "function_code": instance,
+                "status": "PENDING",
+                "output": None
+            }
+            self.tasks.insert(idx, func_meta)
+            return func_meta
+        return build_function
 
     def clear(self):
         """
@@ -37,9 +48,6 @@ class Pipeline:
         single tasks from a pipeline.
         """
         self.tasks = []
-
-    def visualize(self):
-        pass
 
     def run(self, config):
         """
@@ -56,15 +64,53 @@ class Pipeline:
             pipe.run(config)
 
         Returns:
-            [{'task': 'one', 'output': 2}, {'task': 'two', 'output': 3}]
-        """
-        task_outputs = []
-        for task in self.tasks:
-            name = task.__name__
-            task_outputs.append(
+            {'total_tasks': 2,
+             'pipeline_id': '9381b42f-147f-4c8d-908b-4ab7f3f73c64',
+             'started': datetime.datetime(2019, 7, 8, 22, 13, 3, 981497),
+             'tasks': [
                 {
-                    "task": name,
-                    "output": task(**config[name])
-                }
-            )
-        return task_outputs
+                    'task_id': 'e28f4ba4-d392-4ffc-bd0e-d1b43d1bff0a',
+                    'function_name': 'uno',
+                    'function_code': <function __main__.uno(uno)>,
+                    'status': 'SUCCEEDED',
+                    'output': 'this is 1',
+                    'started': datetime.datetime(2019, 7, 8, 22, 13, 3, 981508),
+                    'ended': datetime.datetime(2019, 7, 8, 22, 13, 3, 981515),
+                    'total_duration_seconds': 7e-06},
+                    ...
+              ],
+             'ended': datetime.datetime(2019, 7, 8, 22, 13, 3, 981531),
+             'total_duration_seconds': 3.4e-05
+            }
+        """
+        pipeline_output = {
+            "total_tasks": len(self.tasks),
+            "pipeline_id": str(uuid4()),
+            "started": dt.datetime.now(),
+            "tasks": None
+        }
+
+        for i in range(pipeline_output['total_tasks']):
+            self.tasks[i]['status'] = "RUNNING"
+            task_start = dt.datetime.now()
+            try:
+                self.tasks[i]['started'] = task_start.isoformat()
+                self.tasks[i]['output'] = self.tasks[i]['function_code'](
+                    **config[self.tasks[i]['function_name']]
+                )
+                self.tasks[i]['status'] = "SUCCEEDED"
+                task_end = dt.datetime.now()
+                self.tasks[i]['ended'] = task_end.isoformat()
+                self.tasks[i]['duration_microseconds'] = (
+                    (task_end - task_start).microseconds
+                )
+            except Exception as e:
+                self.tasks[i]['status'] = "FAILED"
+                raise Exception(e)
+
+        pipeline_output['ended'] = dt.datetime.now()
+        pipeline_output['tasks'] = self.tasks
+        pipeline_output['duration_microseconds'] = (
+            (pipeline_output['ended'] - pipeline_output['started']).microseconds
+        )
+        return pipeline_output
